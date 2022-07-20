da228f44b832abc9c1d29422843503399cbaad75
IdrilXu <98962023+IdrilXu@users.noreply.github.com>
2022-07-20 14:03:47 +0100

    parser_
    Add restrictions of parse_S
    Add domain list to store parser output

--- parser_.py
+++ parser_.py
@@ -1,43 +1,53 @@
 from tokenizer import *
-
+import re
 
 class Parser:
     def __init__(self, tokens):
-        self.tokens = tokens
-        self.tokens = Tokenizer('a b( c ) d( e f( g h( + ) ) i j( k l( + ) m n o( p + q( + ) ) r ) )')
+        # self.tokens = tokens
+        self.tokens = Tokenizer(tokens)
 
-    def parse_S(self):
-        while self.tokens:
-            self.parse_D(self.tokens)
+    def parse_S(self, domains):
+        while self.tokens:  # while True
+            first = next(self.tokens)
+            if re.match(Tokenizer.CLOSE, first):# d(S) loop ends with )
+                break
+            else:
+                self.tokens.pushback(first)
+            self.parse_D(domains)
         else:
             raise Exception('Invalid Syntax')
-            #empty with 'space' only？
 
-    def parse_D(self):
+
+    def parse_D(self, domains: list):
         first = next(self.tokens)
-        sum = first
-        if first == Tokenizer.DOMAIN:
-            following = next(first)
-            if following == Tokenizer.OPEN:
-                sum += following
-                self.parse_S(next(self.tokens)) #d(S)
-                sum += next(following)
-                assert next(self.tokens) == Tokenizer.CLOSE
-                sum += Tokenizer.CLOSE
-                print(sum)
-            elif following == Tokenizer.CLOSE or Tokenizer.PLUS:
-                Tokenizer.pushback(following)
+        if re.match(Tokenizer.DOMAIN, first):
+            following = next(self.tokens)
+            if re.match(Tokenizer.OPEN, following):
+                inner_sequence = []
+                self.parse_S(inner_sequence)
+                domains.append(inner_sequence)
+
+            # elif re.match(Tokenizer.OPEN, following) or re.match(Tokenizer.PLUS, following):
+            #     self.token.pushback(following)
             else:
-                print(sum)
-        elif first == Tokenizer.PLUS:
-            print(sum)
-        elif first == Tokenizer.OPEN or Tokenizer.CLOSE:
+                self.tokens.pushback(following)
+                domains.append(f'single domain {first}')
+
+        elif re.match(Tokenizer.PLUS, first):
+            domains.append(f'strand break')
+
+        elif re.match(Tokenizer.CLOSE, first):
+            pass
+
+        elif re.match(Tokenizer.OPEN, first) or re.match(Tokenizer.CLOSE, first):
             raise ValueError('Invalid Syntax')
         else:
             Tokenizer.pushback(first)
 
 if __name__ == '__main__':
-    parsers = Parser(Tokenizer.tokens)
-    print(parsers)
+    tokens = 'a b( c ) d( e f( g h( + ) ) i j( k l( + ) m n o( p + q( + ) ) r ) )'
+    parsers = Parser(tokens)
+    parsers.parse_S()
+    print(list(parsers.tokens))
     #tree?
     #input these structure representatives into SVG
